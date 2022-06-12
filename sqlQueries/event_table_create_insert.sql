7b39398b61d55a95f3e2a9289417c245bdc5d807
revky <filip.chwalinski@gmail.com>
2022-06-12 20:44:43 +0200

    sql query separate

--- sqlQueries/event_table_create_insert.sql
+++ sqlQueries/event_table_create_insert.sql
@@ -22,16 +22,3 @@ VALUES
 '2022-05-31 18:00:00',
 'Yoyoyoyo',
 1);
-
-use events;
-
-CREATE TABLE user
-(
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  login VARCHAR(50) NOT NULL UNIQUE,
-  email VARCHAR(100) NOT NULL UNIQUE,
-  is_admin bit NOT NULL,
-  password VARCHAR(50) NOT NULL,
-  is_active bit NOT NULL,
-  allow_notifications bit NOT NULL
-);
